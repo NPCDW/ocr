15f7f6b94c37f91087865392c58f4fb693975570
cyl14082 <cyl14082@cnki.net>
2022-01-11 13:42:20 +0800

    tencent ocr

--- src/services/tencent.js
+++ src/services/tencent.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export function ocr(headers, payloadStr) {
-    return axios.post('/tencentCloud/', payloadStr,{headers})
+    return axios.post('/tencentCloudOcr/', payloadStr,{headers})
 }
